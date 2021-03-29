85079464e08455db2aa9f43a77a4a5dfb933b2ed
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-29 11:35:26 -0600

    commit tree 4cf617304c6a4d715ea617faff147eb8c376c5b6

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,8 +3,16 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div div className="movies">
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+          <div key={index}>
+              <h3>Name: {movie.title}</h3>
+              <p>Length: {movie.time}</p>
+              <p>Genres: {movie.genres}</p>
+              <p>Metascore: {movie.metascore}</p>
+          </div>
+      ))}
     </div>
   );
 };
